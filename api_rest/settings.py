42694b783be4be504d807ce0111c6d8572ac14f4
felixmorales747 <felixmorales747@gmail.com>
2022-03-17 09:26:05 -0400

    feat: add requirements.txt

--- api_rest/settings.py
+++ api_rest/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'api_rest',
-        'HOST': 'winhost',
+        'HOST': 'localhost',
         'USER': 'postgres',
         'PASSWORD': 'djrex747',
         'PORT': '5432',
